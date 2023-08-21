4bcc033ac178f9ec063822b469bec039b70a7e41
tvbavaro <69318905+tvbavaro@users.noreply.github.com>
2023-08-21 20:10:45 +0300

    add Vuex

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './styles.scss'
 import App from './App.vue'
 import { router } from './router'
+import { store } from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app')
