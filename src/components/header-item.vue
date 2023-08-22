588d9f1b23c70ac218a459191c5f6a624afef4a5
tvbavaro <69318905+tvbavaro@users.noreply.github.com>
2023-08-22 21:33:02 +0300

    set header grid

--- src/components/header-item.vue
+++ src/components/header-item.vue
@@ -7,28 +7,33 @@
                         <img class="header__logo" src="@assets/logo.png" alt="logo">
                         <span class="header__brand-name">InfoDefense</span>
                     </div>
-                <div class="header__search">
-                    <img class="header__search-icon" src="@assets/24x24/search.svg" alt="">
-                    <span class="header__search-text">Search</span>
-                </div>
-                <div class="header__filters">
-                    <img class="header__filters-icon" src="@assets/24x24/filter.svg" alt="">
-                    <span class="header__filters-text">Filters</span>
-                </div>
+                    <div class="header__action-wrapper">
+                        <div class="header__search">
+                            <img class="header__search-icon" src="@assets/24x24/search.svg" alt="">
+                            <span class="header__search-text">Search</span>
+                        </div>
+                        <div class="header__filters">
+                            <img class="header__filters-icon" src="@assets/24x24/filter.svg" alt="">
+                            <span class="header__filters-text">Filters</span>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="header__social">
                     <div class="header__social-wrapper">
-                        <img class="header__twitter-icon" src="@assets/24x24/twitter.svg" alt="">
-                        <img class="header__telegram-icon" src="@assets/24x24/telegram.svg" alt="">
-                        <img class="header__youtube-icon" src="@assets/24x24/youtube.svg" alt="">
+                    <img class="header__twitter-icon" src="@assets/24x24/twitter.svg" alt="">
+                    <img class="header__telegram-icon" src="@assets/24x24/telegram.svg" alt="">
+                    <img class="header__youtube-icon" src="@assets/24x24/youtube.svg" alt="">
                     </div>
-                    
                 </div>
                 <div class="header__navigation">
-                    <div class="header__burger-wrapper">
-                        <span class="header__burger-line"></span>
+                    <div class="header__navigation-wrapper">
+                        <div class="header__burger-wrapper">
+                            <span class="header__burger-line"></span>
+                        </div>
+                        <span class="header__menu-text">Menu</span>
                     </div>
-                    <span class="header__menu-text">Menu</span>
+
                 </div>
             </div>
         </div>
