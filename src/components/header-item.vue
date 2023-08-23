88644f0865bb417627eb600b4fc74139dc135ad8
tvbavaro <69318905+tvbavaro@users.noreply.github.com>
2023-08-23 19:00:26 +0300

    add search and menu

--- src/components/header-item.vue
+++ src/components/header-item.vue
@@ -3,43 +3,106 @@
         <div class="header__wrapper">
             <div class="header__grid">
                 <div class="header__basic">
-                    <div class="header__logo-wrapper">
-                        <img class="header__logo" src="@assets/logo.png" alt="logo">
-                        <span class="header__brand-name">InfoDefense</span>
+                    <div class="logo logo__wrapper">
+                        <img class="logo__img" src="@assets/logo.png" alt="logo">
+                        <span class="logo__name">InfoDefense</span>
                     </div>
                     <div class="header__actions-wrapper">
-                        <div class="header__search">
-                        <div class="header__search-wrapper">
-                            <img class="header__search-icon" src="@assets/24x24/search.svg" alt="">
-                            <span class="header__search-text">Search</span>
+                        <div class="search header__search" :class="{'header__search_active': togglers.isSearchOpen}" @click="openSearch">
+                            <div class="search__wrapper">
+                                <img class="search__icon" src="@assets/24x24/search.svg" alt="">
+                                <span class="search__text">Search</span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="header__filters">
-                        <div class="header__filters-wrapper">
-                            <img class="header__filters-icon" src="@assets/24x24/filter.svg" alt="">
-                            <span class="header__filters-text">Filters</span>
+                        <div class="filters header__filters">
+                            <div class="filters__wrapper">
+                                <img class="filters__icon" src="@assets/24x24/filter.svg" alt="">
+                                <span class="filters__text">Filters</span>
+                            </div>
                         </div>
-                    </div>
                     </div>
 
                 </div>
-                <div class="header__social">
-                    <img class="header__twitter-icon" src="@assets/24x24/twitter.svg" alt="">
-                    <img class="header__telegram-icon" src="@assets/24x24/telegram.svg" alt="">
-                    <img class="header__youtube-icon" src="@assets/24x24/youtube.svg" alt="">
+                <div class="social header__social">
+                    <img class="social__twitter-icon" src="@assets/24x24/twitter.svg" alt="">
+                    <img class="social__telegram-icon" src="@assets/24x24/telegram.svg" alt="">
+                    <img class="social__youtube-icon" src="@assets/24x24/youtube.svg" alt="">
                 </div>
-                <div class="header__navigation">
-                    <div class="header__navigation-wrapper">
-                        <div class="header__burger-wrapper">
-                            <span class="header__burger-line"></span>
+                <div class="navigation header__navigation" :class="{ 'header__navigation_active': togglers.isMenuOpen }"
+                    @click="openMenu">
+                    <div class="navigation__wrapper">
+                        <div class="burger header__burger-wrapper">
+                            <span class="burger__line" :class="{ 'burger__line_active': togglers.isMenuOpen }"></span>
                         </div>
-                        <span class="header__menu-text">Menu</span>
+                        <span class="navigation__text">Menu</span>
                     </div>
+                </div>
+            </div>
+            <div class="menu menu__wrapper" :class="{ 'menu__wrapper_active': togglers.isMenuOpen }">
+                <nav class="menu__grid">
+                    <ul class="menu__column">
+                        <li class="menu__item"><span class="menu__header">News</span></li>
+                        <li class="menu__item"><span class="menu__text">Home</span></li>
+                        <li class="menu__item"><span class="menu__text">Today</span></li>
+                        <li class="menu__item"><span class="menu__text">This week/Last week</span></li>
+                        <li class="menu__item"><span class="menu__text">Nov/Oct/Sept/Aug/Jun/Jul</span></li>
+                        <li class="menu__item"><span class="menu__text">2021/2022</span></li>
+                    </ul>
+                    <ul class="menu__column">
+                        <li class="menu__item"><span class="menu__header">Popular tags</span></li>
+                        <li class="menu__item"><span class="menu__text">#Kherson</span></li>
+                        <li class="menu__item"><span class="menu__text">#Evacuation</span></li>
+                        <li class="menu__item"><span class="menu__text">#war_in_Ukraine</span></li>
+                        <li class="menu__item"><span class="menu__text">#Russian_troops</span></li>
+                        <li class="menu__item"><span class="menu__text">#Genocide2014History</span></li>
+                    </ul>
+                    <ul class="menu__column">
+                        <li class="menu__item"><span class="menu__header">About InfoDefence</span></li>
+                        <li class="menu__item"><span class="menu__text">Project</span></li>
+                        <li class="menu__item"><span class="menu__text">Team</span></li>
+                        <li class="menu__item"><span class="menu__text">Donate</span></li>
+                        <li class="menu__item"><span class="menu__text">Contacts</span></li>
+                    </ul>
+                    <ul class="menu__column">
+                        <li class="menu__item"><span class="menu__header">Stay Connected</span></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="search-menu search-menu__wrapper" :class="{ 'search-menu__wrapper_active': togglers.isSearchOpen }">
+                <div class="search-menu__input-wrapper">
+                    <input class="search-menu__input" type="text" placeholder="Search" />
+                    <img class="search-menu__icon" src="@assets/24x24/search.svg" alt="">
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
-
+export default {
+    data() {
+        return {
+            togglers: {
+                isMenuOpen: false,
+                isSearchOpen: false
+            }
+        }
+    },
+    methods: {
+        closeAllModals() {
+            for(let key in this.togglers) {
+                if(this.togglers[key]) {
+                    this.togglers[key] = false;
+                }
+            }
+        },
+        openMenu() {
+            console.log(Object.entries(this.togglers.isMenuOpen));
+            this.togglers.isMenuOpen = !this.togglers.isMenuOpen;
+        },
+        openSearch() {
+            //this.closeAllModals();
+            this.togglers.isSearchOpen = !this.togglers.isSearchOpen;
+        }
+    }
+}
 </script>
