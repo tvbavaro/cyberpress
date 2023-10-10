5a4a7188b6a684a733e4b6cbc4b2c2e368d35fe2
tvbavaro <69318905+tvbavaro@users.noreply.github.com>
2023-10-10 12:15:22 +0300

    build gh-pages002ver

---
+++ dist/assets/ProjectView-6f7d371f.js
@@ -0,0 +1,1 @@
+import{p as e}from"./project-grid-99a11876.js";import{_ as t,r,o as c,c as p}from"./index-9b247221.js";import"./heading-item-c229fb46.js";const n={components:{projectGrid:e}};function s(a,i,_,m,d,f){const o=r("projectGrid");return c(),p(o,{typePage:"project",typePreview:"donate"})}const $=t(n,[["render",s]]);export{$ as default};
