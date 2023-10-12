d2d74e596e264ef393c7c0a557f52f4f390aa44f
tvbavaro <69318905+tvbavaro@users.noreply.github.com>
2023-10-12 17:04:01 +0300

    build for ver 011

---
+++ dist/assets/heading-item-1ca0f996.js
@@ -0,0 +1,1 @@
+import{_ as t,o as s,a,b as o,e as n}from"./index-4156febd.js";const c={props:{title:{type:String}}},i={class:"heading"},r={class:"heading__text"};function _(d,p,e,h,l,f){return s(),a("div",i,[o("h1",r,n(e.title),1)])}const m=t(c,[["render",_]]);export{m as h};
