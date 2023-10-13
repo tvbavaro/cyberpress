2e22addbabdcfa5c0fcafe31138f0a1bd0bd5b72
tvbavaro <69318905+tvbavaro@users.noreply.github.com>
2023-10-13 16:45:49 +0300

    add favicon

--- dist/assets/DonateView-6072e4cd.js
+++
@@ -1,1 +0,0 @@
-import{p as o}from"./project-grid-af5467d8.js";import{_ as t,r,o as c,c as n}from"./index-4156febd.js";import"./heading-item-1ca0f996.js";const p={components:{projectGrid:o}};function s(a,i,_,m,d,f){const e=r("projectGrid");return c(),n(e,{typePage:"donate",typePreview:"team"})}const j=t(p,[["render",s]]);export{j as default};
---
+++ dist/assets/TeamView-4f453b06.js
@@ -0,0 +1,1 @@
+import{p as o}from"./project-grid-4723afaf.js";import{_ as t,r,o as c,c as p}from"./index-823b624b.js";import"./heading-item-c9218a83.js";const n={components:{projectGrid:o}};function s(a,i,m,_,d,f){const e=r("projectGrid");return c(),p(e,{typePage:"team",typePreview:"project"})}const $=t(n,[["render",s]]);export{$ as default};
