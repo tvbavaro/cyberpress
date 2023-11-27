a84745a6504813c29dc9a9db629905220a7e5351
tvbavaro <69318905+tvbavaro@users.noreply.github.com>
2023-11-27 10:58:23 +0300

    build

---
+++ assets/MainView-ea12d771.js
@@ -0,0 +1,1 @@
+import{_ as n,n as o,r as s,o as r,c as t}from"./index-a6a66771.js";const c={components:{newsGrid:o}};function a(p,_,i,d,f,m){const e=s("newsGrid");return r(),t(e)}const u=n(c,[["render",a]]);export{u as default};
--- assets/heading-item-285f7999.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as s,a,b as o,e as n}from"./index-cf7b8c24.js";const c={props:{title:{type:String}}},i={class:"heading"},r={class:"heading__text"};function _(d,p,e,h,l,f){return s(),a("div",i,[o("h1",r,n(e.title),1)])}const m=t(c,[["render",_]]);export{m as h};
